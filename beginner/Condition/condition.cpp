2ebdc3f11625cc7b1052cf4d742a0cba3d1d3245
0xsadik <torikussadik404@gmail.com>
2022-11-23 22:30:10 +0600

    +

--- beginner/Condition/condition.cpp
+++ beginner/Condition/condition.cpp
@@ -1,69 +1,19 @@
-// #include<iostream>
-// using namespace std;
-
-// int main()
-// {
-// 	// #ifndef TORIKUS
-// 	// 	freopen("input.txt", "r", stdin);
-// 	// 	freopen("output.txt", "w", stdout);
-// 	// #endif
-
-// 	// ------------------------------------
-
-
-// 	int savings;
-// 	cin >> savings;
-// 	// if(savings > 5000) {
-// 	// 	cout << "He will date Neha!" << endl;
-// 	// }
-// 	// else {
-// 	// 	cout << "He will date Rashmi!" << endl;
-// 	// }
-	
-
-// 	if(savings > 5000) {
-// 		cout << "He will date Neha!" << endl;
-// 	}
-// 	else if(savings > 2000) {
-// 		cout << "He will date Rashmi!" << endl;
-// 	}
-// 	else {
-// 		cout << "He will go with his friends!";
-// 	}
-	
-
-
-// }
-
-
-
-
-
-
 #include<iostream>
 using namespace std;
 
-int main() {
-	int a, b, c;
-	// cin >> a >> b >> c;
-	a = 10;
-	b = 17;
-	c = 15;
-
-	if (a > b) {
-		if (a > c) {
-			cout << a << endl;
-		}
-		else {
-			cout << c << endl;
-		}
+int main()
+{
+	int savings;
+	savings = 1200;
+	
+	if(savings > 5000) {
+		cout << "He will date Neha!" << endl;
+	}
+	else if(savings > 2000) {
+		cout << "He will date Rashmi!" << endl;
 	}
 	else {
-		if (b > c) {
-			cout << b << endl;
-		}
-		else {
-			cout << c << endl;
-		}
+		cout << "He will go with his friends!" << endl;
 	}
 }
+
